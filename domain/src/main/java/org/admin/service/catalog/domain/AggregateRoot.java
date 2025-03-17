860e9d928284cd6a8a735c4e96e9023173862407
alantrs <alanaraujotrs@gmail.com>
2025-03-17 15:17:10 -0300

    feat: adicionado as classes do DDD

---
+++ domain/src/main/java/org/admin/service/catalog/domain/AggregateRoot.java
@@ -0,0 +1,8 @@
+package org.admin.service.catalog.domain;
+
+public class AggregateRoot<ID extends Identifier> extends Entity<ID>{
+
+    protected AggregateRoot(final ID id) {
+        super(id);
+    }
+}
